7b5b3ca5da6ea8c01b46cce78b381299c784ef21
Pavel Kamburov <pkamburov89@gmail.com>
2024-07-27 16:03:23 +0300

    Add username to user register

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -4,4 +4,4 @@ const BASE_URL = 'http://localhost:3030/users';
 
 export const login = (email, password) => requester.post(`${BASE_URL}/login`, {email, password});
 
-export const register = (email, password) => requester.post(`${BASE_URL}/register`, {email, password});
+export const register = (email, password, username) => requester.post(`${BASE_URL}/register`, {email, password, username});
