b57900cea15ad49fe5068e10fe763ba434b3c129
Pavel Kamburov <pkamburov89@gmail.com>
2024-07-27 16:04:02 +0300

    Migrate to data/collection

--- client/src/api/bonsai-api.js
+++ client/src/api/bonsai-api.js
@@ -1,38 +1,23 @@
 import * as request from './requester';
 
-const BASE_URL = 'http://localhost:3030/jsonstore/bonsai';
+const BASE_URL = 'http://localhost:3030/data';
 
 export const getAll = async () => {
-    const result = await request.get(`${BASE_URL}/species`);
+    const result = await request.get(`${BASE_URL}/species?select=_id%2Cname%2CnameBinominal%2CimageUrl%2Ccategory`);
     
     const guides = Object.values(result);
 
     return guides;
 }
 
-export const getFeatured = async () => {
-    const result = await request.get(`${BASE_URL}/featured`);
-    
-    const featured = Object.values(result);
-
-    return featured;
-}
-
 export const getTreeDetails = async (treeId) => {
-    const result = await request.get(`${BASE_URL}/details/${treeId}`);
+    const result = await request.get(`${BASE_URL}/species/${treeId}`);
 
     const details = Object.values(result);
     return details;
 }
 
 export const getOne = (pageId) => request.get(`${BASE_URL}/details/${pageId}`);
-
-export const getAllPages = async () => {
-    const result = await request.get(`${BASE_URL}/pages`);
-
-    const pages = Object.values(result);
-    return pages;
-}
 
 export const getFeaturedPages = async () => {
     const result = await request.get(`${BASE_URL}/featuredPages`);
@@ -47,8 +32,6 @@ export const createGuide = (guideData) => request.post(`${BASE_URL}`, guideData)
 export const bonsaiApi = {
     getAll,
     getOne,
-    getAllPages,
-    getFeatured,
     getFeaturedPages,
     getPage,
     createGuide
