57d9abbf67c269aaa660905eea6ae870decddf0d
Pavel Kamburov <pkamburov89@gmail.com>
2024-07-29 13:53:28 +0300

    Add page Bonsai care information

--- client/src/components/pages/PageBonsaiCare.jsx
+++ client/src/components/pages/PageBonsaiCare.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import * as bonsaiAPI from '../../api/bonsai-api';
 import HeroSection from "../hero-section/HeroSection";
 import PageShortDescription from "./PageShortDescription";
+import { InfoSection } from "../info-section/InfoSection";
+import { InfoAccordion } from "../accordion/InfoAccordion";
 
 export default function PageBonsaiCare() {
 
@@ -13,11 +15,19 @@ export default function PageBonsaiCare() {
             .then(result => setPage(result));
     }, []);
 
-    return (
-        <>
-            <HeroSection page={page} />
-            <PageShortDescription page={page} />
-        </>
-        
-    )
+    if (page.guidelines) {
+        const guidelines = page.guidelines.slice(0, 3);
+        const accordionInfo = page.guidelines.slice(3, page.guidelines.length);
+
+        return (
+            <>
+                <HeroSection page={page} />
+                <PageShortDescription page={page} />
+                <InfoSection guidelines={guidelines} />
+                <InfoAccordion guidelines={accordionInfo} />
+            </>
+            
+        )
+    }
+
 }
