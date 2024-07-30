8797eecd2a504ee926f0c3f18ad9aaf2604a8f1f
Pavel Kamburov <pkamburov89@gmail.com>
2024-07-30 21:38:39 +0300

    Create logout

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -5,3 +5,5 @@ const BASE_URL = 'http://localhost:3030/users';
 export const login = (email, password) => requester.post(`${BASE_URL}/login`, {email, password});
 
 export const register = (email, password, username) => requester.post(`${BASE_URL}/register`, {email, password, username});
+
+export const logout = () => requester.get(`${BASE_URL}/logout`);
