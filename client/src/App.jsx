4bb560cfcef0772704ed85d572cdd6b5c44fa701
Pavel Kamburov <pkamburov89@gmail.com>
2024-08-01 22:14:23 +0300

    Add remove accessToken before unload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 
 import { AuthContextProvider } from "./contexts/AuthContext"
@@ -14,8 +15,21 @@ import PageBonsaiStyling from "./components/pages/PageBonsaiStyling"
 import GuideDetails from "./components/guide-details/GuideDetails"
 import Register from "./components/register/Register"
 import Logout from "./components/logout/Logout"
+import GuideEdit from "./components/guide-edit/GuideEdit"
 
 function App() {
+    useEffect(() => {
+        const handleBeforeUnload = () => {
+          localStorage.removeItem('accessToken');
+        };
+    
+        window.addEventListener('beforeunload', handleBeforeUnload);
+    
+        return () => {
+          window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+      }, []);
+
     return (
         <AuthContextProvider>
             <div id="wrapper" className="bg-white">
@@ -30,6 +44,7 @@ function App() {
                     <Route path="/tree-species" element={<Guides />} />
                     <Route path="/create" element={<CreateGuide />} />
                     <Route path="/species/:guideId" element={<GuideDetails />} />
+                    <Route path="/edit/:guideId" element={<GuideEdit />} />
                     <Route path="*" element={<NotFound />} />
                     <Route path="/species/*" element={<NotFound />} />
                 </Routes>
