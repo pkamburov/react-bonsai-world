1a5e90c29ced2272ed390f777dce6b932713f9fd
Pavel Kamburov <pkamburov89@gmail.com>
2024-08-01 22:13:42 +0300

    Edit GuideOwnerSection

--- client/src/components/guide-details/GuideOwnerSection.jsx
+++ client/src/components/guide-details/GuideOwnerSection.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function GuideOwnerSection(username) {
+
+    const { guideId } = useParams();
+
     return (
         <div className="flex-grid columns-2 gap-8 max-w-6xl m-auto">
             <div className="flex-grid columns-2">
-                <Link to="/edit/:guideId" className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase mx-5">
+                <Link to={`/edit/${guideId}`} className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase mx-5">
                     Edit
                 </Link>
-                <Link to="/delete/:guideId" className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase">
+                <Link to={`/delete/${guideId}`} className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase">
                     Delete
                 </Link>
             </div>
