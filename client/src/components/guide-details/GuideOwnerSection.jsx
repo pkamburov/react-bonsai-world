5a7cc5e58c3ed0a519f88136948765ddaa0df2c8
Pavel Kamburov <pkamburov89@gmail.com>
2024-08-03 13:34:17 +0300

    Create guideDeleteHandler

--- client/src/components/guide-details/GuideOwnerSection.jsx
+++ client/src/components/guide-details/GuideOwnerSection.jsx
@@ -1,8 +1,18 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { bonsaiApi } from "../../api/bonsai-api";
 
-export default function GuideOwnerSection(username) {
-
+export default function GuideOwnerSection() {
     const { guideId } = useParams();
+    const navigate = useNavigate();
+    
+    const guideDeleteHandler = async () => {
+        try {
+            await bonsaiApi.deleteGuide(guideId);
+            navigate('/species');
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return (
         <div className="flex-grid columns-2 gap-8 max-w-6xl m-auto">
@@ -10,9 +20,9 @@ export default function GuideOwnerSection(username) {
                 <Link to={`/edit/${guideId}`} className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase mx-5">
                     Edit
                 </Link>
-                <Link to={`/delete/${guideId}`} className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase">
+                <button onClick={guideDeleteHandler} className="px-4 py-2 text-[0.92em] font-medium border-2 border-slate-300 text-gray-700 hover:bg-slate-100 rounded-lg uppercase">
                     Delete
-                </Link>
+                </button>
             </div>
             <div className="float-right font-body text-dark-gray text-[16px]">
                 Likes:
