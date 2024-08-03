89a4f5e7ae93fab8d01a21b89377bca0361c369f
Pavel Kamburov <pkamburov89@gmail.com>
2024-08-03 13:33:37 +0300

    Fix PrivateGuard

--- client/src/components/common/PrivateGuard.jsx
+++ client/src/components/common/PrivateGuard.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
-import {Navigate, Outlet} from 'react-router-dom'
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom'
 
 import { AuthContext } from '../../contexts/AuthContext';
 
 export default function PrivateGuard() {
-    const { isAuthenticated } = useState(AuthContext);
+    const { isAuthenticated } = useContext(AuthContext);
 
-    return isAuthenticated
-        ? <Outlet />
-        : <Navigate to="/login" />
+    if (isAuthenticated) {
+        return <Outlet />
+    } else {
+        return <Navigate to="/login" />
+    }
 }
