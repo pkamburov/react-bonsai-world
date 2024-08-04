73f78fbe1c712849fd023453091e913a17db122f
Pavel Kamburov <pkamburov89@gmail.com>
2024-08-04 23:35:44 +0300

    Add ThemeProvider and fix CSS

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import './index.css'
 
+import { ThemeProvider } from "@material-tailwind/react";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-        <BrowserRouter>
+    <BrowserRouter>
+        <ThemeProvider>
             <App />
-        </BrowserRouter>
+        </ThemeProvider>
+    </BrowserRouter>
     /* </React.StrictMode> */
 )
